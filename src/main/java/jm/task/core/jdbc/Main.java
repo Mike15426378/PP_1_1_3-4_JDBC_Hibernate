fced6d4a127d7becdca1d5ab3390301c92694cac
Mike15426378 / <Ravaga228>
2022-02-08 17:57:24 +0300

    v1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
+    private static UserService us = new UserServiceImpl();
+    private static User user1 = new User("Ivan", "Ivanov", (byte) 40);
+    private static User user2 = new User("Petr", "Petrov", (byte) 30);
+    private static User user3 = new User("Sanya", "Sankov", (byte) 25);
+    private static User user4 = new User("Vasya", "Vaskin", (byte) 20);
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    us.createUsersTable();
+
+    us.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+    us.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+    us.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+    us.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+
+    us.getAllUsers();
+
+    us.cleanUsersTable();
+
+    us.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,85 @@
 package jm.task.core.jdbc.dao;
 
+import com.sun.source.tree.StatementTree;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+
+    private final Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS test.users " +
+                    "(id int not null auto_increment, name VARCHAR(30), " +
+                    "lastname VARCHAR(30), age numeric, PRIMARY KEY (id))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate("DROP TABLE IF EXISTS test.users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement ps = connection
+                .prepareStatement("INSERT INTO test.users(name, lastname, age) VALUES(?, ?, ?)")) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate("DELETE FROM test.users WHERE id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = new ArrayList<>();
+
+        try (Statement st = connection.createStatement()) {
+            ResultSet rs = st.executeQuery("SELECT id, name, lastName, age FROM test.users");
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastName"));
+                user.setAge(rs.getByte("age"));
+                allUsers.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allUsers;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate("TRUNCATE test.users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
